a4ca6573cd503dcb969d4cfa8d4f9374a3ddac00
Ozoniuss <qscxzsewqazxcde@gmail.com>
2025-04-26 17:54:52 +0300

    Use table driven tests

--- test/config_test.go
+++ test/config_test.go
@@ -4,6 +4,8 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -15,28 +17,11 @@ type TestConfig1 struct {
 	Retries int
 }
 
-func TestGeneratedConfig1(t *testing.T) {
-
-	t.Setenv("TESTCONFIG1_APPNAME", "SuperApp")
-	t.Setenv("TESTCONFIG1_DEBUG", "true")
-	t.Setenv("TESTCONFIG1_TIMEOUT", "5s")
-	t.Setenv("TESTCONFIG1_RETRIES", "3")
-
-	expected := TestConfig1{
-		AppName: "SuperApp",
-		Debug:   true,
-		Timeout: 5 * time.Second,
-		Retries: 3,
-	}
-
-	actual, err := LoadTestConfig1()
-	if err != nil {
-		t.Fatalf("expected no error, got: %s", err.Error())
-	}
-
-	if expected != actual {
-		t.Fatalf("expected configs to be equal: want %+v, got %+v", expected, actual)
-	}
+type TestConfigCopy struct {
+	AppName string
+	Debug   bool
+	Timeout time.Duration
+	Retries int
 }
 
 type TestConfigInts struct {
@@ -46,27 +31,14 @@ type TestConfigInts struct {
 	Int64Val int64
 }
 
-func TestGeneratedConfigInts(t *testing.T) {
-	t.Setenv("TESTCONFIGINTS_INT8VAL", "8")
-	t.Setenv("TESTCONFIGINTS_INT16VAL", "1600")
-	t.Setenv("TESTCONFIGINTS_INT32VAL", "320000")
-	t.Setenv("TESTCONFIGINTS_INT64VAL", "6400000000")
+type Nested struct {
+	InnerStr  string
+	InnerBool bool
+}
 
-	expected := TestConfigInts{
-		Int8Val:  8,
-		Int16Val: 1600,
-		Int32Val: 320000,
-		Int64Val: 6400000000,
-	}
-
-	actual, err := LoadTestConfigInts()
-	if err != nil {
-		t.Fatalf("expected no error, got: %s", err)
-	}
-
-	if expected != actual {
-		t.Fatalf("expected: %+v, got: %+v", expected, actual)
-	}
+type TestConfigNested struct {
+	AppName string
+	Nested  Nested
 }
 
 type TestConfigMixed struct {
@@ -83,86 +55,160 @@ type TestConfigUints struct {
 	Uint32Val uint32
 	Uint64Val uint64
 }
-
-func TestGeneratedConfigUints(t *testing.T) {
-	t.Setenv("TESTCONFIGUINTS_UINT8VAL", "8")
-	t.Setenv("TESTCONFIGUINTS_UINT16VAL", "1600")
-	t.Setenv("TESTCONFIGUINTS_UINT32VAL", "320000")
-	t.Setenv("TESTCONFIGUINTS_UINT64VAL", "6400000000")
-
-	expected := TestConfigUints{
-		Uint8Val:  8,
-		Uint16Val: 1600,
-		Uint32Val: 320000,
-		Uint64Val: 6400000000,
-	}
-
-	actual, err := LoadTestConfigUints()
-	if err != nil {
-		t.Fatalf("expected no error, got: %s", err)
-	}
-	if expected != actual {
-		t.Fatalf("expected: %+v, got: %+v", expected, actual)
-	}
-}
-
 type TestConfigFloats struct {
 	Float32Val float32
 	Float64Val float64
 }
 
-func TestGeneratedConfigFloats(t *testing.T) {
-	t.Setenv("TESTCONFIGFLOATS_FLOAT32VAL", "1.23")
-	t.Setenv("TESTCONFIGFLOATS_FLOAT64VAL", "3.1415")
-
-	expected := TestConfigFloats{
-		Float32Val: 1.23,
-		Float64Val: 3.1415,
+func callLoadFuncByName(t *testing.T, tc TestCase) (interface{}, error) {
+	t.Helper()
+	loadFunc := reflect.ValueOf(loadFuncRegistry[tc.LoadFuncName])
+	if !loadFunc.IsValid() {
+		t.Fatalf("load function not valid: %s", loadFunc)
 	}
 
-	actual, err := LoadTestConfigFloats()
-	if err != nil {
-		t.Fatalf("expected no error, got: %s", err)
+	results := loadFunc.Call(nil)
+	actual := results[0].Interface()
+	fmt.Println("actual", actual)
+	var err error
+	if results[1].Interface() != nil {
+		err = results[1].Interface().(error)
 	}
-
-	// Loose float comparisons
-	if delta := actual.Float32Val - expected.Float32Val; delta < -0.001 || delta > 0.001 {
-		t.Errorf("float32 mismatch: got %f, want %f", actual.Float32Val, expected.Float32Val)
-	}
-	if delta := actual.Float64Val - expected.Float64Val; delta < -0.000001 || delta > 0.000001 {
-		t.Errorf("float64 mismatch: got %f, want %f", actual.Float64Val, expected.Float64Val)
-	}
+	return actual, err
 }
 
-type Nested struct {
-	InnerStr  string
-	InnerBool bool
-}
+var loadFuncRegistry map[string]any
 
-type TestConfigNested struct {
-	AppName string
-	Nested  Nested
-}
-
-func TestGeneratedConfigNested(t *testing.T) {
-	t.Setenv("TESTCONFIGNESTED_APPNAME", "NestedApp")
-	t.Setenv("TESTCONFIGNESTED_NESTED_INNERSTR", "hello")
-	t.Setenv("TESTCONFIGNESTED_NESTED_INNERBOOL", "true")
-
-	expected := TestConfigNested{
-		AppName: "NestedApp",
-		Nested: Nested{
-			InnerStr:  "hello",
-			InnerBool: true,
+func getTestCases() []TestCase {
+	tcs := []TestCase{
+		{
+			StructName: "TestConfig1",
+			SetEnvs: func(t *testing.T) {
+				t.Setenv("TESTCONFIG1_APPNAME", "SuperApp")
+				t.Setenv("TESTCONFIG1_DEBUG", "true")
+				t.Setenv("TESTCONFIG1_TIMEOUT", "5s")
+				t.Setenv("TESTCONFIG1_RETRIES", "3")
+			},
+			Expected: TestConfig1{
+				AppName: "SuperApp",
+				Debug:   true,
+				Timeout: 5 * time.Second,
+				Retries: 3,
+			},
+		},
+		{
+			StructName: "TestConfigCopy",
+			SetEnvs: func(t *testing.T) {
+				t.Setenv("TESTCONFIGCOPY_APPNAME", "SuperApp")
+				t.Setenv("TESTCONFIGCOPY_DEBUG", "true")
+				t.Setenv("TESTCONFIGCOPY_TIMEOUT", "5s")
+				t.Setenv("TESTCONFIGCOPY_RETRIES", "3")
+			},
+			Expected: TestConfigCopy{
+				AppName: "SuperApp",
+				Debug:   true,
+				Timeout: 5 * time.Second,
+				Retries: 3,
+			},
+		},
+		{
+			StructName: "TestConfigInts",
+			SetEnvs: func(t *testing.T) {
+				t.Setenv("TESTCONFIGINTS_INT8VAL", "8")
+				t.Setenv("TESTCONFIGINTS_INT16VAL", "1600")
+				t.Setenv("TESTCONFIGINTS_INT32VAL", "320000")
+				t.Setenv("TESTCONFIGINTS_INT64VAL", "6400000000")
+			},
+			Expected: TestConfigInts{
+				Int8Val:  8,
+				Int16Val: 1600,
+				Int32Val: 320000,
+				Int64Val: 6400000000,
+			},
+		},
+		{
+			StructName: "TestConfigUints",
+			SetEnvs: func(t *testing.T) {
+				t.Setenv("TESTCONFIGUINTS_UINT8VAL", "8")
+				t.Setenv("TESTCONFIGUINTS_UINT16VAL", "1600")
+				t.Setenv("TESTCONFIGUINTS_UINT32VAL", "320000")
+				t.Setenv("TESTCONFIGUINTS_UINT64VAL", "6400000000")
+			},
+			Expected: TestConfigUints{
+				Uint8Val:  8,
+				Uint16Val: 1600,
+				Uint32Val: 320000,
+				Uint64Val: 6400000000,
+			},
+		},
+		{
+			StructName: "TestConfigFloats",
+			SetEnvs: func(t *testing.T) {
+				t.Setenv("TESTCONFIGFLOATS_FLOAT32VAL", "1.23")
+				t.Setenv("TESTCONFIGFLOATS_FLOAT64VAL", "3.1415")
+			},
+			Expected: TestConfigFloats{
+				Float32Val: 1.23,
+				Float64Val: 3.1415,
+			},
+		},
+		{
+			StructName: "TestConfigNested",
+			SetEnvs: func(t *testing.T) {
+				t.Setenv("TESTCONFIGNESTED_APPNAME", "NestedApp")
+				t.Setenv("TESTCONFIGNESTED_NESTED_INNERSTR", "hello")
+				t.Setenv("TESTCONFIGNESTED_NESTED_INNERBOOL", "true")
+			},
+			Expected: TestConfigNested{
+				AppName: "NestedApp",
+				Nested: Nested{
+					InnerStr:  "hello",
+					InnerBool: true,
+				},
+			},
 		},
 	}
 
-	actual, err := LoadTestConfigNested()
-	if err != nil {
-		t.Fatalf("expected no error, got: %s", err)
+	for i := range tcs {
+		tcs[i].LoadFuncName = "Load" + tcs[i].StructName
 	}
 
-	if expected != actual {
-		t.Fatalf("expected: %+v, got: %+v", expected, actual)
+	// Build the registry manually (unfortunately Go can't discover this automatically)
+	loadFuncRegistry = map[string]any{
+		"LoadTestConfig1":      LoadTestConfig1,
+		"LoadTestConfigCopy":   LoadTestConfigCopy,
+		"LoadTestConfigInts":   LoadTestConfigInts,
+		"LoadTestConfigUints":  LoadTestConfigUints,
+		"LoadTestConfigFloats": LoadTestConfigFloats,
+		"LoadTestConfigNested": LoadTestConfigNested,
+	}
+
+	return tcs
+}
+
+type TestCase struct {
+	StructName   string
+	SetEnvs      func(t *testing.T)
+	Expected     any
+	LoadFuncName string
+	IsError      bool
+}
+
+func TestGenerator(t *testing.T) {
+	testcases := getTestCases()
+	for _, tc := range testcases {
+		t.Run(tc.StructName, func(t *testing.T) {
+			tc.SetEnvs(t)
+			config, err := callLoadFuncByName(t, tc)
+			if !tc.IsError && err != nil {
+				t.Errorf("unexpected error when parsing config: %s", err)
+			}
+			if tc.IsError && err != nil {
+				t.Errorf("expected errror to occur during parse: %s", err)
+			}
+			if !reflect.DeepEqual(tc.Expected, config) {
+				t.Errorf("expected %+v (%v), got %+v (%v)\n", tc.Expected, reflect.TypeOf(tc.Expected), config, reflect.TypeOf(config))
+			}
+		})
 	}
 }
--- test/generate.go
+++ test/generate.go
@@ -4,6 +4,7 @@ import "testmain/github/genconfig"
 
 func main() {
 	genconfig.GenerateConfigLoader("TESTCONFIG1", "TestConfig1", "config_test.go", "c1_test.go", "", false, "testcases")
+	genconfig.GenerateConfigLoader("TESTCONFIGCOPY", "TestConfigCopy", "config_test.go", "c1_1_test.go", "", false, "testcases")
 	genconfig.GenerateConfigLoader("TESTCONFIGINTS", "TestConfigInts", "config_test.go", "c2_test.go", "", false, "testcases")
 	genconfig.GenerateConfigLoader("TESTCONFIGUINTS", "TestConfigUints", "config_test.go", "c3_test.go", "", false, "testcases")
 	genconfig.GenerateConfigLoader("TESTCONFIGFLOATS", "TestConfigFloats", "config_test.go", "c4_test.go", "", false, "testcases")
